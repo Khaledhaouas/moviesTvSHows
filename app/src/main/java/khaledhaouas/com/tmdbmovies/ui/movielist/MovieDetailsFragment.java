9f3ec4dfac4e95bafc1013ddd5ce827af498d33c
khaledhoues <khaledhoues@yahoo.fr>
2019-01-30 01:31:57 +0900

    hide actionbar progress on movie details layout

--- app/src/main/java/khaledhaouas/com/tmdbmovies/ui/movielist/MovieDetailsFragment.java
+++ app/src/main/java/khaledhaouas/com/tmdbmovies/ui/movielist/MovieDetailsFragment.java
@@ -1,13 +1,18 @@
 package khaledhaouas.com.tmdbmovies.ui.movielist;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 import khaledhaouas.com.tmdbmovies.R;
 
@@ -24,7 +29,10 @@ public class MovieDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.movie_details_fragment, container, false);
+
+        View v = inflater.inflate(R.layout.movie_details_fragment, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        return v;
     }
 
     @Override
@@ -35,6 +43,20 @@ public class MovieDetailsFragment extends Fragment {
 
         mViewModel.getPopular();
 
+        RatingBar rating = getActivity().findViewById(R.id.rating);
+        initRatingBar(rating);
+
+
+    }
+
+    private void initRatingBar(RatingBar rating) {
+        LayerDrawable stars = (LayerDrawable) rating.getProgressDrawable();
+        //Color filled stars
+        stars.getDrawable(2).setColorFilter(ContextCompat.getColor(getActivity(), R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        //Color half filled stars
+        stars.getDrawable(1).setColorFilter(ContextCompat.getColor(getActivity(), R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        //Color empty stars
+        stars.getDrawable(0).setColorFilter(ContextCompat.getColor(getActivity(), R.color.grey), PorterDuff.Mode.SRC_IN);
     }
 
 }
