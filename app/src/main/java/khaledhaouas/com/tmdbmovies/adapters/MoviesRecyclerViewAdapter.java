663006f32524878130c12d41ba6a74b5162c8b9a
khaledhoues <khaledhoues@yahoo.fr>
2019-02-14 01:59:33 +0900

    add animation to movies adapter

--- app/src/main/java/khaledhaouas/com/tmdbmovies/adapters/MoviesRecyclerViewAdapter.java
+++ app/src/main/java/khaledhaouas/com/tmdbmovies/adapters/MoviesRecyclerViewAdapter.java
@@ -6,8 +6,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -57,6 +60,8 @@ public class MoviesRecyclerViewAdapter extends RecyclerView.Adapter<MoviesRecycl
         holder.mTxtMovieReviewNbrs.setText(mData.get(position).getReviewNbrs() + "");
         holder.mTxtMovieReleaseDate.setText(mData.get(position).getReleaseDate());
 
+        setAnimation(holder.mMovieLayout);
+
 
     }
 
@@ -64,6 +69,12 @@ public class MoviesRecyclerViewAdapter extends RecyclerView.Adapter<MoviesRecycl
     @Override
     public int getItemCount() {
         return mData.size();
+    }
+
+    private void setAnimation(View viewToAnimate) {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        Animation animation = AnimationUtils.loadAnimation(viewToAnimate.getContext(), android.R.anim.slide_in_left);
+        viewToAnimate.startAnimation(animation);
     }
 
     //     convenience method for getting data at click position
@@ -89,6 +100,7 @@ public class MoviesRecyclerViewAdapter extends RecyclerView.Adapter<MoviesRecycl
         RatingBar mRtMovieRating;
         TextView mTxtMovieReviewNbrs;
         TextView mTxtMovieReleaseDate;
+        RelativeLayout mMovieLayout;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -98,6 +110,7 @@ public class MoviesRecyclerViewAdapter extends RecyclerView.Adapter<MoviesRecycl
             mTxtMovieReviewNbrs = itemView.findViewById(R.id.txt_movie_reviews_nbre);
             mTxtMovieReleaseDate = itemView.findViewById(R.id.txt_movie_release_date);
             mRtMovieRating = itemView.findViewById(R.id.txt_movie_rating);
+            mMovieLayout = itemView.findViewById(R.id.layout_movie_item);
 
             itemView.setOnClickListener(this);
         }
